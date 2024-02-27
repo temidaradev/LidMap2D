5a1228173f17b19d5f498bb9048efc72236d1632
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-02-27 20:41:51 +0300

    fixed

--- source/designer.go
+++ source/designer.go
@@ -1,4 +1,18 @@
 package game
 
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
 type Designer struct {
+}
+
+func (d *Designer) Draw(screen *ebiten.Image) {
+	screen.Fill(color.White)
+}
+
+func (d *Designer) Update() error {
+	return nil
 }
